4b1acf19b58954b16a90a0184dba24ac3004f427
Shendrit-Hy <shendrit.hyseni2004@gmail.com>
2024-01-09 16:47:13 +0100

    Plot sene

--- JavaScript/check-if-signed-in.js
+++ JavaScript/check-if-signed-in.js
@@ -1,5 +1,21 @@
+function ifLogIn() {
+  if (localStorage.getItem('logged')){
+    const signButton = document.getElementById('signup-and-login')
+    const logout = document.getElementById('logout')
+    const signName = document.getElementById('name-of-user')
+    signName.innerHTML = localStorage.getItem('userName')
+    signButton.style.display = 'none';
+    logout.style.display = 'flex'
+  }
+  else{
+    const logout = document.getElementById('logout')
+    logout.style.display = 'none'
+    console.log("Asdasdasd")
+  }
+}
+
 function signOut(){
   localStorage.removeItem('logged')
-  window.location.href = 'signUp.html'
+  window.location.href = 'home.html'
 }
 
